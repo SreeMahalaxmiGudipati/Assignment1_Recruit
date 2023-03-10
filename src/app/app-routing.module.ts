6aded1f3f559642a52b3f8563130b6337d1d60cd
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-10 21:59:34 +0530

    adding navbar for every component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrudoperationsComponent } from './crudoperations/crudoperations.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'',component:HomeComponent},
+ {path:'parent',component:HomeComponent},
+  {path:'',
+      children:[
+        {path:'parent/home',component:HomeComponent},
+        {path:'parent/crudoperations',component:CrudoperationsComponent},
+        {path:'parent/list',component:ListComponent},
+      ]},
+  // {path:'home',component:HomeComponent},
+  //      {path:'crudoperations',component:CrudoperationsComponent},
+  //    {path:'list',component:ListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudoperationsComponent } from './crudoperations/crudoperations.component';
+import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { ParentComponent } from './parent/parent.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    HomeComponent,
+    ParentComponent,
+    CrudoperationsComponent
   ],
   imports: [
     BrowserModule,
